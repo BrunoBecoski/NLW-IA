6183cd995fb412bb7c53d52aa40ef51b5ebef589
Bruno Becoski <brunobecoski@gmail.com>
2023-09-16 14:34:57 -0300

    Criando o componente prompt-select

--- upload-ai-web/src/app.tsx
+++ upload-ai-web/src/app.tsx
@@ -14,8 +14,13 @@ import {
 import { Slider } from "./components/ui/slider";
 
 import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
 
 export function App() {
+  function handlePromptSelected(template: string) {
+    console.log(template)    
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -67,15 +72,7 @@ export function App() {
             
             <div className="space-y-2">
               <Label>Prompt</Label> 
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
 
             <div className="space-y-2">
