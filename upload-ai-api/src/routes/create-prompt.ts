c9ab9dd8b7dd9201b2512a327fa3273c48e56b54
Bruno Becoski <brunobecoski@gmail.com>
2023-09-28 20:37:18 -0300

    Atualizando a rota create-prompt

--- upload-ai-api/src/routes/create-prompt.ts
+++ upload-ai-api/src/routes/create-prompt.ts
@@ -11,6 +11,26 @@ export async function createPrompt(app: FastifyInstance) {
     
     const { title, template } = bodySchema.parse(req.body)
 
+    const responseTitle = await prisma.prompt.findFirst({
+      where: {
+        title,
+      }
+    })
+
+    if(responseTitle) {
+      return reply.status(409).send({ message: 'Title already exist.' })
+    }
+
+    const responseTemplate = await prisma.prompt.findFirst({
+      where: {
+        template,
+      }
+    })
+
+    if(responseTemplate) {
+      return reply.status(409).send({ message: 'Prompt already exist.' })
+    }
+
     await prisma.prompt.create({
       data: {
         title,
